1bc9bd7c1ef88f438a76847e27e79ea877a095f8
Chris Huang <chenhsiu@gmail.com>
2021-01-08 12:17:59 +0800

    lake use ink6

--- best.sh
+++ best.sh
@@ -5,7 +5,7 @@
 ./WCT.py --content input/woman.jpg --style style/pencil2.jpg
 ./WCT.py --content input/woman.jpg --style style/water.jpg
 
-./WCT.py --content input/lake.jpg --style style/ink4.jpg
+./WCT.py --content input/lake.jpg --style style/ink6.jpg
 ./WCT.py --content input/lake.jpg --style style/oil5.jpg
 ./WCT.py --content input/lake.jpg --style style/pencil2.jpg
 ./WCT.py --content input/lake.jpg --style style/water3.jpg

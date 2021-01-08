def7a9be34816d3e1b0710d15afca4825e588cd7
Chris Huang <chenhsiu@gmail.com>
2021-01-08 12:11:40 +0800

    add best.sh

---
+++ best.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./WCT.py --content input/woman.jpg --style style/ink4.jpg
+./WCT.py --content input/woman.jpg --style style/oil6.jpg
+./WCT.py --content input/woman.jpg --style style/pencil2.jpg
+./WCT.py --content input/woman.jpg --style style/water.jpg
+
+./WCT.py --content input/lake.jpg --style style/ink4.jpg
+./WCT.py --content input/lake.jpg --style style/oil5.jpg
+./WCT.py --content input/lake.jpg --style style/pencil2.jpg
+./WCT.py --content input/lake.jpg --style style/water3.jpg
+
+./WCT.py --content input/street.jpg --style style/ink4.jpg
+./WCT.py --content input/street.jpg --style style/oil2.jpg

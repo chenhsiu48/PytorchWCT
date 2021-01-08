2e4b1fa2c47ec9495dc3275849c78b6c4359f8fc
Chris Huang <chenhsiu@gmail.com>
2021-01-08 17:16:49 +0800

    revise mk_video.sh

--- mk_video.sh
+++ mk_video.sh
@@ -2,5 +2,6 @@
 
 for s in oil1 pencil2 water2 ink4; do
     ./WCT.py --style style/$s.jpg input/video/frames/*.jpg
-    /usr/bin/ffmpeg -y -framerate 60 -i output/ntu%04d-$s.png -c:v h264_nvenc -b:v 1400k -vf "fps=60,format=yuv420p" -aspect 16:9 ntu-$s.mp4
+    /usr/bin/ffmpeg -y -framerate 60 -i output/ntu%04d-$s.png -c:v h264_nvenc -b:v 1400k -vf "fps=60,format=yuv420p" -aspect 16:9 output/ntu-$s.mp4
+    rm output/ntu*-$s.png
 done
